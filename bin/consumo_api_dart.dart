488b3a48bdc1630753f89b6d76b63ae5982154f1
Ulysses Martins Siqueira <33982499+Ulysses007@users.noreply.github.com>
2023-06-28 18:54:46 -0300

    first commit

---
+++ bin/consumo_api_dart.dart
@@ -0,0 +1,5 @@
+import 'package:consumo_api_dart/consumo_api_dart.dart' as consumo_api_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${consumo_api_dart.calculate()}!');
+}
---
+++ test/consumo_api_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:consumo_api_dart/consumo_api_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
